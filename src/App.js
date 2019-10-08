5785d0e7b76e5e2163f6be37dab88c920922a2d6
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-08 12:51:16 -0600

    Add react router to render in app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Landing from './Landing';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -6,13 +7,38 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      isLoading: true,
+      movies: [],
+      favorites: [],
+      userName: '',
+      userQuote: '',
+      userSkill: '',
     }
   }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/films/')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          isLoading: false,
+          movies: data
+      })
+    })
+  }
+
   render() {
     return (
       <div className='App'>
-        <h2>App</h2>
+        <Router>
+          <h2>App</h2>
+          <Switch>
+            <Route exact path='/' component={Landing}>
+              <Landing />
+            </Route>
+          </Switch>
+        </Router>  
       </div>
     );
   }
