679af75d3b5e4660d2c6d72a797e99d17ba4e256
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-08 21:14:19 -0600

    Add rendering and base styling for movie cards

--- src/Movies.js
+++ src/Movies.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 import './Movies.css';
 
 
 
 const Movies = (props) => {
-  const movieCards = props.map(movie => {
-    <li>
+  const movieCards = props.movies.map(movie => {
+    return (<li className='li li--movie'>
       <MovieCard
+        key={movie.episodeId}
         title={movie.title}
-        episodeID={movie.episodeId}
+        episodeId={movie.episodeId}
         releaseDate={movie.releaseDate}
         characters={movie.characters}
         openingCrawl={movie.openingCrawl}
       />
-    </li>  
+    </li>)  
   })
   return (
-    <section>
-      <ul>
+    <section className='section section--movie-list'>
+      <ul className='ul ul--movie-list'>
         {movieCards}
       </ul>  
     </section>
