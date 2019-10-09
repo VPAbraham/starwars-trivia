dcd9c96c8c637802c143a0adab53da8d290f7116
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-09 14:05:28 -0600

    Add boiler for characters component

---
+++ src/Characters/Characters.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CharacterCard from '../CharacterCard/CharacterCard';
+import './Characters.css';
+
+
+
+const Characters = (props) => {
+  const characterCards = props.characters.map(character => {
+    return (<li key={character.episodeId} className='li li--character'>
+      <CharacterCard
+
+      />
+    </li>)
+  });
+  return (
+    <section className='section section--character-list'>
+      <ul className='ul ul--character-list'>
+        {characterCards}
+      </ul>
+    </section>
+  )
+}
+
+
+export default Characters;
