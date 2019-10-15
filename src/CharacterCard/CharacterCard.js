3fdd8d36e7c38359866d168219d9e7c090538db4
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-15 08:57:45 -0600

    Add button for adding/removing favorite & reduced character fetch to first ten

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './CharacterCard.css';
-import propTypes from 'prop-types'
+import favorite from '../images/lightsaber.png';
+import notFav from '../images/lightsaber-disabled.png';
+import propTypes from 'prop-types';
 
 
-const CharacterCard = ({ name, homeworld, species, relatedFilms, isFavorite }) => {
+const CharacterCard = (props) => {
+  const { name, homeworld, species, relatedFilms, isFavorite } = props
   const films = relatedFilms.map(film => <li>{film}</li>)
+  const favIcon = notFav ? notFav : favorite;
+  const favMethod = isFavorite ? props.removeFav : props.addFav;
   return (
     <div className='div div--character-card'>
+      <img className='img--fav-icon' src={favIcon} onClick={event => favMethod({ name, homeworld, species, relatedFilms, isFavorite })}/>
       <h3 className='h3 h3--character-card'>{name}</h3>
       <ul>
         <p className='p p--char-data'>Homeworld</p>
@@ -15,7 +21,7 @@ const CharacterCard = ({ name, homeworld, species, relatedFilms, isFavorite }) =
         <p className='p p--char-data'>Species</p>
         <li>{species}</li>
         <p className='p p--char-data'>Appears in:</p>
-        <li>{films}</li>
+        <ul>{films}</ul>
       </ul>  
     </div>
   )
@@ -29,5 +35,5 @@ CharacterCard.propTypes = {
   homeworldPop: propTypes.number,
   species: propTypes.string,
   relatedFilms: propTypes.array,
-  isFavorite: propTypes.string
+  isFavorite: propTypes.bool
 }
