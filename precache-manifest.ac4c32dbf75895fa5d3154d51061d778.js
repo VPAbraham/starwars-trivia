b87f2c834234f0e23977fadffd64694bfcee7e99
VPAbraham <vpa456@gmail.com>
2019-10-16 11:02:34 -0600

    Updates

---
+++ precache-manifest.ac4c32dbf75895fa5d3154d51061d778.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0949c37fa465d033a99cf7b1f7f95d",
+    "url": "/starwars-trivia/index.html"
+  },
+  {
+    "revision": "16b1a82bdc794286f4bf",
+    "url": "/starwars-trivia/static/css/main.9a4e0d0a.chunk.css"
+  },
+  {
+    "revision": "45879f4676ed95c16b19",
+    "url": "/starwars-trivia/static/js/2.845f9e1d.chunk.js"
+  },
+  {
+    "revision": "16b1a82bdc794286f4bf",
+    "url": "/starwars-trivia/static/js/main.faeda25f.chunk.js"
+  },
+  {
+    "revision": "a7e3ad24c03d000175d7",
+    "url": "/starwars-trivia/static/js/runtime-main.e49bdc03.js"
+  },
+  {
+    "revision": "dd06c3cd3bb5e5e00deac7246437861e",
+    "url": "/starwars-trivia/static/media/SWloadingIcon.dd06c3cd.gif"
+  },
+  {
+    "revision": "5979693511d7bad37bf9e45e58cc6623",
+    "url": "/starwars-trivia/static/media/StarWarsLogo.59796935.png"
+  },
+  {
+    "revision": "fb9a14d5a0f1124cb007dbeae460d408",
+    "url": "/starwars-trivia/static/media/cockpit.fb9a14d5.png"
+  },
+  {
+    "revision": "20cb58c1451f27bc0b4738151e8b911b",
+    "url": "/starwars-trivia/static/media/episode-1.20cb58c1.png"
+  },
+  {
+    "revision": "669d3b1b794ee261168dcf37e7dffe6d",
+    "url": "/starwars-trivia/static/media/episode-2.669d3b1b.png"
+  },
+  {
+    "revision": "bfae345dcc6c90898e4221db8de5c0a9",
+    "url": "/starwars-trivia/static/media/episode-3.bfae345d.png"
+  },
+  {
+    "revision": "c28b4d479085f0f912ae9c21a21209af",
+    "url": "/starwars-trivia/static/media/episode-4.c28b4d47.png"
+  },
+  {
+    "revision": "0095edd6ad985dc5b32ee580d946b9ac",
+    "url": "/starwars-trivia/static/media/episode-5.0095edd6.png"
+  },
+  {
+    "revision": "5337a25da8c28c9aef7e514fe2685a5b",
+    "url": "/starwars-trivia/static/media/episode-6.5337a25d.png"
+  },
+  {
+    "revision": "26eeed3cd054d9f7a7ad00d58947e700",
+    "url": "/starwars-trivia/static/media/episode-7.26eeed3c.png"
+  },
+  {
+    "revision": "1742b3755c3831db39e3e176795f17ea",
+    "url": "/starwars-trivia/static/media/lightsaber.1742b375.png"
+  },
+  {
+    "revision": "75c1b027571c0b586a577ac3f8321a86",
+    "url": "/starwars-trivia/static/media/starry-night.75c1b027.jpg"
+  }
+]);
